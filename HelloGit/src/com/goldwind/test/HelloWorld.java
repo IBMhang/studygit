62239485663b8ea76678a8cd7c1efcb7b23825df
zhaoyaohang <15933236193@163.com>
2017-04-07 16:19:38 +0800

    xxx

--- HelloGit/src/com/goldwind/test/HelloWorld.java
+++ HelloGit/src/com/goldwind/test/HelloWorld.java
@@ -3,6 +3,7 @@ package com.goldwind.test;
 public class HelloWorld {
 
 	private String name;
+	private String age;
 	public static void main(String[] args) {
 		System.out.println("helloworld");
 		
