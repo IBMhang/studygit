fc68f62cacf7fb8ee80cb75b25056a452f69c915
zhaoyaohang <15933236193@163.com>
2017-04-09 11:28:16 +0800

    test2

--- HelloGit/src/com/goldwind/test2/HelloWorld.java
+++ HelloGit/src/com/goldwind/test2/HelloWorld.java
@@ -3,6 +3,7 @@ package com.goldwind.test2;
 public class HelloWorld {
 
 	private String name;
+	private String test2;
 	public static void main(String[] args) {
 		System.out.println("helloworld");
 		
